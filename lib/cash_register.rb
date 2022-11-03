761e87aebf964539e4d41f49eeebc20474a3071c
Brian Lambert <brianlambert2408@gmail.com>
2022-11-03 13:19:47 +1100

    Completed tasks

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
 
+class CashRegister
+
+    attr_accessor :discount, :total, :items, :last
+
+    def initialize(discount=0)
+        @discount = discount
+        @total = 0
+        @items = []
+    end
+    
+    def add_item title, price, quantity=1
+        @total += price * quantity
+        @last = price * quantity
+        quantity.times { @items << title }
+    end
+
+    def apply_discount
+
+        return "There is no discount to apply." if discount == 0
+
+        @total = @total * (1 - (discount / 100.0 ) )
+        "After the discount, the total comes to $#{total.to_i}."
+    end
+
+    def void_last_transaction
+        @total -= @last
+    end
+
+
+end
